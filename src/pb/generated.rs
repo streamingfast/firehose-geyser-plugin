1d65bb352a18fd8d9027aed31518a20ec7c25b66
Charles Billette <charles@cbillette.com>
2024-10-24 14:53:34 -0400

    Add new protobuf definitions and dependencies
    Included Solana protobuf definitions and generated corresponding Rust structures. Updated Cargo.toml and Cargo.lock with necessary dependencies such as anyhow, cargo-lock, git-version, and vergen.

---
+++ src/pb/generated.rs
@@ -0,0 +1,12 @@
+// @generated
+pub mod sf {
+    pub mod solana {
+        pub mod r#type {
+            // @@protoc_insertion_point(attribute:sf.solana.type.v1)
+            pub mod v1 {
+                include!("sf.solana.type.v1.rs");
+                // @@protoc_insertion_point(sf.solana.type.v1)
+            }
+        }
+    }
+}
